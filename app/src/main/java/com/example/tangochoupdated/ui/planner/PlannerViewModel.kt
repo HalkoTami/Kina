83933f9e0ca9f3c8aa9c734786e8e05b30a47897
unknown <halko316hvb@gmail.com>
2022-05-17 16:05:26 +0900

    bottomnavigationview

--- app/src/main/java/com/example/tangochoupdated/ui/planner/PlannerViewModel.kt
+++ app/src/main/java/com/example/tangochoupdated/ui/planner/PlannerViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.tangochoupdated.ui.notifications
+package com.example.tangochoupdated.ui.planner
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class PlannerViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is planner Fragment"
     }
     val text: LiveData<String> = _text
 }
