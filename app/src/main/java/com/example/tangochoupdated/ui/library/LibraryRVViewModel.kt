548efbf6e51e0e3bfa960ffda858228e3770945c
unknown <halko316hvb@gmail.com>
2022-06-10 11:00:43 +0900

    Rvのデータをfetchできるように

--- app/src/main/java/com/example/tangochoupdated/ui/library/LibraryRVViewModel.kt
+++ app/src/main/java/com/example/tangochoupdated/ui/library/LibraryRVViewModel.kt
@@ -8,21 +8,28 @@ import com.example.tangochoupdated.room.rvclasses.LibraryRV
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
-class  LibraryViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Library Fragment"
-    }
-    val text: LiveData<String> = _text
-}
 class LibraryRVViewModel(private val repository: MyRoomRepository) : ViewModel(){
-    var _listData = MutableLiveData<List<LibraryRV>>()
-    suspend  fun get ():List<LibraryRV>{
-        return repository.getLibRVCover(null)
+    private var _listData = MutableLiveData<List<LibraryRV>>()
+    fun fetchListData(id:Int?){
+        var a :List<LibraryRV>
+        viewModelScope.launch {
+            withContext(Dispatchers.Default){
+                val list = repository.getLibRVCover(id)
+                return@withContext  fetchData(list)
+            }
+        }
+
     }
 
+    private fun fetchData(a:List<LibraryRV>){
+        _listData.value = a
+    }
 
-
+    override fun onCleared() {
+        super.onCleared()
+        viewModelScope.cancel()
+    }
     /**
      * Launching a new coroutine to insert the data in a non-blocking way
      */
