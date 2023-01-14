86263da568c93cb56caef15de06d1d13357aa7c5
HalkoTami <halko316hvb@gmail.com>
2023-01-14 14:18:37 +0900

    guideのclicklistener とそれだけが入っていたファイル削除

--- app/src/main/java/com/koronnu/kina/ui/listener/libraryBaseFragment/PopUpJumpToGuideCL.kt
+++
@@ -1,9 +0,0 @@
-package com.koronnu.kina.ui.listener.libraryBaseFragment
-
-import android.view.View
-
-class PopUpJumpToGuideCL(): View.OnClickListener{
-        override fun onClick(v: View?) {
-
-        }
-    }
